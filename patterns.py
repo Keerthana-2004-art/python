1eccdb250ca872d19bacf6902a0a53e029f92ff3
Keerthana-2004-art <keerthanaboyina@gmail.com>
2025-07-31 11:22:43 +0530

    Update patterns.py

--- patterns.py
+++ patterns.py
@@ -39,3 +39,12 @@ for i in range(n):
         else:
             print(" ",end=" ")
     print()
+
+n=int(input("enter the size"))
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==n-1 or i+j==n-1:
+            print("*",end=' ')
+        else:
+            print(" ",end=" ")
+    print()
